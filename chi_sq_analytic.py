3a4e5938d2aefc624f7bfd3c020d5943f217e1cc
Andrew Fowlie <andrew.j.fowlie@qq.com>
2021-03-23 11:45:06 +0800

    pc for chi2 example

--- chi_sq_analytic.py
+++ chi_sq_analytic.py
@@ -1,16 +1,17 @@
-from scipy.stats import norm, chi2
+from scipy.stats import chi2
+from scipy.special import ndtri
 
-from p_value import dynesty, brute, mn, Result
+from p_value import dynesty, brute, mn, pc, Result
 
-observed = 30.
-n_dim = 5
+observed = 110.
+n_dim = 30
 
 def analytic_p_value(observed_, n_dim):
     p = chi2.sf(observed_, n_dim)
     return Result(p, 0, None)
 
 def transform(cube):
-    return norm.ppf(cube)
+    return ndtri(cube)
     
 def test_statistic(observed_):
     return (observed_**2).sum()
@@ -18,4 +19,5 @@ def test_statistic(observed_):
 print(analytic_p_value(observed, n_dim))
 print(brute(test_statistic, transform, n_dim, observed, n=50000))
 #print(dynesty(test_statistic, transform, n_dim, observed, n_live=100))
-print(mn(test_statistic, transform, n_dim, observed, n_live=100))
+#print(mn(test_statistic, transform, n_dim, observed, n_live=100))
+print(pc(test_statistic, transform, n_dim, observed, n_live=100))
