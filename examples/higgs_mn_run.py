98231affabbd03d1a6613cddb1101f4b52a281b4
Sebastian Hoof <hoof@uni-goettingen.de>
2021-04-08 10:51:31 +0200

    Added minimal Higgs MN example.

---
+++ examples/higgs_mn_run.py
@@ -0,0 +1,22 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.stats import poisson
+
+sys.path.append("./../p_value")
+import higgs_functions as higgs
+import ns
+
+targ = 9.2
+n_dim_higgs = len(higgs.expected_bkg)
+
+def transform(cube):
+    return poisson.ppf(cube, mu=higgs.expected_bkg)
+
+r1, r2 = ns.mn(higgs.nested_ts, transform, n_dim_higgs, targ, n_live=100, basename="chains/mn_1_", resume=False, ev_data=True)
+
+plt.plot(r2[0], r2[1])
+plt.show()
+
+print("P-value at TS = {:.2f}: {:.2e} ({:.1f} sigma)".format(targ, 10**r1.log10_pvalue, r1.significance))
