62668ec83cb23692ced1829040a623f56c2835f7
Sebastian Hoof <hoof@uni-goettingen.de>
2021-04-08 18:55:30 +0200

    Added min working ex for nested errors check.

--- examples/nested_errors.py
+++ examples/nested_errors.py
@@ -7,6 +7,7 @@ from scipy.stats import chi2
 script_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, script_dir+'/../p_value/')
 from ns import mn
+from brute import brute
 
 
 def transform(cube):
@@ -15,14 +16,13 @@ def transform(cube):
 def test_statistic(observed_):
     return (observed_**2).sum()
 
-def get_mn_pval_single(unique_id, file_root="./mn", n_dim=2, target_obs=9.0):
-   obs = chi2.isf(chi2.sf(3.0**2, df=1), df=n_dim)
-   temp = mn(test_statistic, transform, n_dim, target_obs, n_live=100, basename=file_root+"_{:d}".format(int(unique_id)), resume=False, ev_data=False)
-   return np.array([temp.error_log10_pvalue, temp.log10_pvalue])
+def get_mn_pval_single(unique_id, file_root="./mn", n_dim=2, target_obs=9.0, n_live=100, save_all=False):
+   bname = file_root
+   if (save_all):
+       bname += "_{:d}".format(int(unique_id))
+   temp = mn(test_statistic, transform, n_dim, target_obs, n_live=n_live, basename=bname, resume=False, ev_data=False)
+   return np.array([temp.log10_pvalue, temp.error_log10_pvalue])
 
-def get_mn_pval(task_id, n_batch_size, n_dim=2, target_obs=9.0):
-   res = []
-   for i in range(n_batch_size):
-      print("Running batch {} in task {}".format(i, task_id))
-      res.append( get_mn_pval_single(task_id*n_batch_size+i, "./mn", n_dim, target_obs) )
-   return np.array(res)
+def get_brute_mc_pval(n_brute_samples=1e6, n_dim=2, target_obs=9.0):
+   temp = brute(test_statistic, transform, n_dim, target_obs, n=int(n_brute_samples))
+   return np.array([temp.log10_pvalue, temp.error_log10_pvalue])
