6758a8f019bdc01cc9bcebbd2fb7dd6f26ebc0e5
Sebastian Hoof <hoof@uni-goettingen.de>
2021-05-07 16:05:16 +0200

    Updated Higgs PCh run file (in particualr no clustering now).

--- examples/mpi_higgs_ns_run.py
+++ examples/mpi_higgs_ns_run.py
@@ -1,21 +1,25 @@
 import sys
+import os
 import numpy as np
 
 from scipy.stats import poisson
 
-sys.path.append("/usr/users/hoof1/ns_for_p_values/p_value")
-sys.path.append("/usr/users/hoof1/ns_for_p_values/examples")
+script_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, script_dir+'/../examples/')
+sys.path.insert(0, script_dir+'/../p_value/')
 import higgs_functions as higgs
 from ns import pc
 
 targ = float(sys.argv[1])
+res = bool(sys.argv[2])
+root = str(sys.argv[3])
 print("Staring run. Target: {}.".format(targ), flush=True)
 n_dim_higgs = len(higgs.expected_bkg)
 
 def transform(cube):
     return poisson.ppf(cube, mu=higgs.expected_bkg)
 
-# r1, r2 = ns.mn(higgs.nested_ts, transform, n_dim_higgs, targ, n_live=100, basename="chains/mn_", resume=False, ev_data=True)
-r1, r2 = pc(higgs.nested_ts, transform, n_dim_higgs, targ, n_live=100, file_root="pc_higgs", feedback=2, resume=True, ev_data=True)
+#r1, r2 = pc(higgs.nested_ts_simple, transform, n_dim_higgs, targ, n_live=100, file_root=root, feedback=2, resume=res, ev_data=True, do_clustering=False)
+r1, r2 = pc(higgs.nested_ts_simple_fast, transform, n_dim_higgs, targ, n_live=100, file_root=root, feedback=2, resume=res, ev_data=True, do_clustering=False)
 
-print("P-value at TS = {:.2f}: {:.2e} ({:.1f} sigma)".format(targ, 10**r1.log10_pvalue, r1.significance))
+print("P-value at TS = {:.2f}: {:.2e} ({:.1f} sigma), {} calls.".format(targ, 10**r1.log10_pvalue, r1.significance, r1.calls))
