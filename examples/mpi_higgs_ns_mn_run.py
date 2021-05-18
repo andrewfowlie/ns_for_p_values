9e6d5df2af8c5056521552b624c2dda6bfc8dde5
Sebastian Hoof <hoof@uni-goettingen.de>
2021-05-18 12:12:32 +0200

    Simplified Higgs functions.

--- examples/mpi_higgs_ns_mn_run.py
+++ examples/mpi_higgs_ns_mn_run.py
@@ -2,8 +2,6 @@ import sys
 import os
 import numpy as np
 
-from scipy.stats import poisson
-
 script_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, script_dir+'/../examples/')
 sys.path.insert(0, script_dir+'/../p_value/')
@@ -12,12 +10,10 @@ from ns import mn
 
 targ = float(sys.argv[1])
 res = bool(sys.argv[2])
-print("Staring run. Target: {}.".format(targ), flush=True)
 n_dim_higgs = len(higgs.expected_bkg)
 
-def transform(cube):
-    return poisson.ppf(cube, mu=higgs.expected_bkg)
+print("Starting run. Target: {}.".format(targ), flush=True)
 
-r1, r2 = mn(higgs.nested_ts_simple, transform, n_dim=n_dim_higgs, observed=targ, n_live=100, basename="chains/mn_sbkg_", resume=res, ev_data=True)
+r1, r2 = mn(higgs.nested_ts_simple, higgs.generate_pseudo_data, n_dim=n_dim_higgs, observed=targ, n_live=100, basename="chains/mn_sbkg_", resume=res, ev_data=True)
 
 print("P-value at TS = {:.2f}: {:.2e} ({:.1f} sigma, {} calls)".format(targ, 10**r1.log10_pvalue, r1.significance, r1.calls))
--- examples/mpi_higgs_ns_pch_run.py
+++ examples/mpi_higgs_ns_pch_run.py
@@ -2,8 +2,6 @@ import sys
 import os
 import numpy as np
 
-from scipy.stats import poisson
-
 script_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, script_dir+'/../examples/')
 sys.path.insert(0, script_dir+'/../p_value/')
@@ -14,14 +12,11 @@ targ = float(sys.argv[1])
 res = bool(sys.argv[2])
 basedir = str(sys.argv[3])+'/chains/'
 root = str(sys.argv[4])
-print("Staring run. Target: {}.".format(targ), flush=True)
 n_dim_higgs = len(higgs.expected_bkg)
 
-def transform(cube):
-    return poisson.ppf(cube, mu=higgs.expected_bkg)
+print("Starting run. Target: {}.".format(targ), flush=True)
 
 kwargs = { 'base_dir': basedir, 'do_clustering': False }
-#r1, r2 = pc(higgs.nested_ts_simple, transform, n_dim_higgs, targ, n_live=100, base_dir=basedir, file_root=root, feedback=2, resume=res, ev_data=True, do_clustering=False)
-r1, r2 = pc(higgs.nested_ts_simple_fast, transform, n_dim_higgs, targ, n_live=100, file_root=root, feedback=2, resume=res, ev_data=True, **kwargs)
+r1, r2 = pc(higgs.nested_ts_simple_fast, higgs.generate_pseudo_data, n_dim_higgs, targ, n_live=100, file_root=root, feedback=2, resume=res, ev_data=True, **kwargs)
 
 print("P-value at TS = {:.2f}: {:.2e} ({:.1f} sigma), {} calls.".format(targ, 10**r1.log10_pvalue, r1.significance, r1.calls))
