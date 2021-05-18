32df47782c5583ea696d8553cb35fa494c824252
Andrew Fowlie <andrew.j.fowlie@qq.com>
2021-05-18 11:46:29 +0800

    keep only one version of mn interface

--- p_value/ns.py
+++ p_value/ns.py
@@ -76,57 +76,6 @@ def dumper(index, observed):
             logging.info("no live points")
     return dumper_
 
-def mn_wrap_loglike(test_statistic, observed, max_calls):
-    """
-    Implement stopping criteria for MN
-    """
-    def wrapped(cube, n_dim, n_params, threshold):
-        cube_arr = np.array([cube[i] for i in range(n_dim)])
-        t = float(test_statistic(cube_arr))
-        wrapped.count += 1
-
-        calls = wrapped.count - wrapped.calls[-1]
-        if calls > max_calls:
-            wrapped.max_calls_exceeded = True
-
-        # force convergence
-        if t > observed or wrapped.max_calls_exceeded:
-            if wrapped.max_calls_exceeded:
-                logging.debug("forcing stop as max calls exceeded")
-            else:
-                logging.debug("forcing stop as max threshold exceeded")
-            t = float(observed)
-
-        if threshold > observed or threshold > wrapped.threshold:
-            wrapped.threshold = threshold
-            wrapped.thresholds.append(wrapped.threshold)
-            wrapped.calls.append(wrapped.count)
-            if wrapped.count > 1000 * wrapped.counter:
-                logging.debug("threshold = {:.5f} (observed = {:.2f}), calls = {:d} (total = {:d})".format(
-                    threshold, observed, int(calls), int(wrapped.count)))
-                wrapped.counter += 1
-        return t
-
-    wrapped.max_calls_exceeded = False
-    wrapped.threshold = -1e30
-    wrapped.thresholds = [-1e30]
-    wrapped.count = 0
-    wrapped.counter = 0
-    wrapped.calls = [0]
-    return wrapped
-
-def mn_wrap_prior(transform):
-    """
-    Safely wrap MN prior
-    """
-    def wrapped(cube, n_dim, n_params):
-        cube_arr = np.array([cube[i] for i in range(n_params)])
-        phys_arr = transform(cube_arr)
-        for i in range(n_params):
-            cube[i] = phys_arr[i]
-
-    return wrapped
-
 def pc_wrap(test_statistic):
     """
     Returns a tuple for PC signature
@@ -140,89 +89,73 @@ def mn(test_statistic, transform, n_dim, observed,
        n_live=100, max_calls=1e10, basename="chains/mn_",
        resume=False, ev_data=False, sampling_efficiency=0.3, **kwargs):
     """
-    Nested sampling with MN
+    Nested sampling with MN.
+
+    Wrappers must be inside scope of function.
     """
-    loglike = mn_wrap_loglike(test_statistic, observed, max_calls)
-    pymultinest.run(loglike,
-                    mn_wrap_prior(transform), n_dim, n_live_points=n_live,
-                    dump_callback=dumper(3, observed),
+    def mn_wrap_prior(cube, ndim, nparams):
+        try:
+            a = np.array([cube[i] for i in range(n_dim)])
+            b = transform(a)
+            for i in range(n_dim):
+                cube[i] = b[i]
+        except Exception as e:
+            raise SystemExit('Error in prior: %s' % e)
+
+    def mn_wrap_loglike(cube, ndim, nparams, threshold):
+        try:
+            a = np.array([cube[i] for i in range(n_dim)])
+            t = float(test_statistic(a))
+
+            if not np.isfinite(t):
+                logging.warning("log-likelihood not finite: %f\n"
+                                "for parameters: %s\n"
+                                "returned very low value instead", t, a)
+                return -1e100
+
+            mn_wrap_loglike.count += 1
+            calls = mn_wrap_loglike.count - mn_wrap_loglike.calls[-1]
+
+            if calls > max_calls:
+                mn_wrap_loglike.max_calls_exceeded = True
+
+            # force convergence
+            if t > observed or mn_wrap_loglike.max_calls_exceeded:
+                if mn_wrap_loglike.max_calls_exceeded:
+                    logging.debug("forcing stop as max calls exceeded")
+                else:
+                    logging.debug("forcing stop as max threshold exceeded")
+                t = float(observed)
+
+            if threshold > observed or threshold > mn_wrap_loglike.threshold:
+                mn_wrap_loglike.threshold = threshold
+                mn_wrap_loglike.thresholds.append(mn_wrap_loglike.threshold)
+                mn_wrap_loglike.calls.append(mn_wrap_loglike.count)
+                if mn_wrap_loglike.count > 1000*mn_wrap_loglike.counter:
+                    logging.debug("threshold = {:.5f} (observed = {:.2f}), calls = {:d} (total = {:d})".format(
+                        threshold, observed, int(calls), int(mn_wrap_loglike.count)))
+                    mn_wrap_loglike.counter += 1
+
+            return t
+
+        except Exception as e:
+            raise SystemExit('Error in test statistic evaluation: %s' % e)
+
+    mn_wrap_loglike.max_calls_exceeded = False
+    mn_wrap_loglike.threshold = -1e30
+    mn_wrap_loglike.thresholds = [-1e30]
+    mn_wrap_loglike.count = 0
+    mn_wrap_loglike.counter = 0
+    mn_wrap_loglike.calls = [0]
+
+    pymultinest.run(mn_wrap_loglike, mn_wrap_prior, n_dim,
+                    n_live_points=n_live,
                     outputfiles_basename=basename,
-                    resume=resume,
+                    dump_callback=dumper(3, observed),
                     importance_nested_sampling=False,
                     sampling_efficiency=sampling_efficiency,
-                    evidence_tolerance=0., **kwargs)
-
-    res, test_statistic, log_x, log_x_delta = analyze_mn_output(observed, root=basename, n_live=n_live)
-
-    if not ev_data:
-        return res
-
-    return res, [test_statistic, log_x, log_x_delta, loglike.thresholds, loglike.calls]
-
-def mn_new(test_statistic, transform, n_dim, observed, n_live=100, max_calls=1e8, basename="chains/mn_", resume=False, ev_data=False, sampling_efficiency=0.3, **kwargs):
-    """
-    Nested sampling with MN
-    """
-
-    def safe_transform(cube, ndim, nparams):
-       try:
-          a = np.array([cube[i] for i in range(n_dim)])
-          b = transform(a)
-          for i in range(n_dim):
-             cube[i] = b[i]
-       except Exception as e:
-             import sys
-             sys.stderr.write('ERROR in prior: %s\n' % e)
-             sys.exit(1) 
-
-    def safe_ts(cube, ndim, nparams, threshold):
-       try:
-          a = np.array([cube[i] for i in range(n_dim)])
-          t = float(test_statistic(a))
-          if not np.isfinite(t):
-             import sys
-             sys.stderr.write('WARNING: loglikelihood not finite: %f\n' % (l))
-             sys.stderr.write('         for parameters: %s\n' % a)
-             sys.stderr.write('         returned very low value instead\n')
-             return -1e100
-          safe_ts.count += 1
-          calls = safe_ts.count - safe_ts.calls[-1]
-          if calls > max_calls:
-             safe_ts.max_calls_exceeded = True
-          # force convergence
-          if t > observed or safe_ts.max_calls_exceeded:
-             if safe_ts.max_calls_exceeded:
-                logging.debug("forcing stop as max calls exceeded")
-             else:
-                logging.debug("forcing stop as max threshold exceeded")
-             t = float(observed)
-          if threshold > observed or threshold > safe_ts.threshold:
-             safe_ts.threshold = threshold
-             safe_ts.thresholds.append(safe_ts.threshold)
-             safe_ts.calls.append(safe_ts.count)
-             if (safe_ts.count > 1000*safe_ts.counter):
-                logging.debug("threshold = {:.5f} (observed = {:.2f}), calls = {:d} (total = {:d})".format(threshold, observed, int(calls), int(safe_ts.count)))
-                safe_ts.counter += 1
-          return t
-       except Exception as e:
-          import sys
-          sys.stderr.write('ERROR in test statistic evaluation: %s\n' % e)
-          sys.exit(1)
-
-    safe_ts.max_calls_exceeded = False
-    safe_ts.threshold = -1e30
-    safe_ts.thresholds = [-1e30]
-    safe_ts.count = 0
-    safe_ts.counter = 0
-    safe_ts.calls = [0]
-
-    run(safe_ts, safe_transform, n_dim,
-          n_live_points=n_live,
-          outputfiles_basename=basename,
-          dump_callback=dumper(3, observed),
-          importance_nested_sampling=False,
-          sampling_efficiency=sampling_efficiency,
-          evidence_tolerance=0., **kwargs)
+                    evidence_tolerance=0.,
+                    resume=resume, **kwargs)
 
     # get number of iterations
     ev_name = "{}ev.dat".format(basename)
@@ -236,7 +169,7 @@ def mn_new(test_statistic, transform, n_dim, observed, n_live=100, max_calls=1e8
     calls = int(line.split()[1])
 
     if not ev_data:
-        return ns_result(n_iter, n_live, calls)
+        return Result.from_ns(n_iter, n_live, calls)
 
     # get ev data
     ev_data = np.genfromtxt(ev_name)
@@ -244,7 +177,7 @@ def mn_new(test_statistic, transform, n_dim, observed, n_live=100, max_calls=1e8
     log_x = -np.arange(0, len(test_statistic), 1.) / n_live
     log_x_delta = np.sqrt(-log_x / n_live)
 
-    return ns_result(n_iter, n_live, calls), [test_statistic, log_x, log_x_delta, safe_ts.thresholds, safe_ts.calls]
+    return Result.from_ns(n_iter, n_live, calls), [test_statistic, log_x, log_x_delta, mn_wrap_loglike.thresholds, mn_wrap_loglike.calls]
 
 def pc(test_statistic, transform, n_dim, observed, n_live=100, file_root="pc_", feedback=0, resume=False, ev_data=False, base_dir='chains/', **kwargs):
     """
